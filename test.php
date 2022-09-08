f67f673bfa008b9092b701a4d35564ddac5f5dc6
AmamiSQ <r_zimmerman@outlook.com>
2022-09-08 12:13:17 +0200

    functions

--- test.php
+++ test.php
@@ -9,39 +9,46 @@
 </head>
 
 <body>
+    <!-- HTML for text boxes and submit buttons -->
     <form action="test.php" method="post">
     Name: <input type="text" name="name"><br>
     Password: <input type="password" name="pass"><br>
     <input type="submit">
     </form>
+
     <!-- start PHP code -->
     <?php
-       error_reporting(E_ERROR | E_PARSE);
-       $user = false;
-       $pass = false;
+       error_reporting(E_ERROR | E_PARSE); //don't print warnings to the screen
+       
+       function login(){
+        $user = false;
+        $pass = false;
 
-       if($_POST['name'] == "testU"){
-            $user = true;
-       }
-       if($_POST['pass'] == "testP"){
-            $pass = true;
+        //check if given username and password are correct
+        if($_POST['name'] == "testU"){
+                $user = true;
+        }
+        if($_POST['pass'] == "testP"){
+                $pass = true;
+        }
+
+        switch([$user, $pass]){
+                case [true, true]:
+                    echo "user logged in";
+                    break;
+                case [true, false]:
+                    echo "password incorrect";
+                    break;
+                default: 
+                    echo "no such user";
+                    break;
+        }
+       };
+       function TFA(){
+        //placeholder
        }
 
-       switch([$user, $pass]){
-            case [true, true]:
-                echo "user logged in";
-                break;
-            case [true, false]:
-                echo "password incorrect";
-                break;
-            case [false, true]:
-                echo "username incorrect";
-                break;
-            default: 
-                echo "no such user";
-                break;
-       }
-        
+       login();
     ?>
    <!-- end PHP code --> 
 </body>
