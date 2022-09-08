7a2e07b3183f2d8dc9ee014e136affc5de8a902a
AmamiSQ <r_zimmerman@outlook.com>
2022-09-08 11:52:41 +0200

    login barebones

--- test.php
+++ test.php
@@ -1,28 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testing</title>
 </head>
+
 <body>
+    <form action="test.php" method="post">
+    Name: <input type="text" name="name"><br>
+    Password: <input type="password" name="pass"><br>
+    <input type="submit">
+    </form>
     <!-- start PHP code -->
     <?php
-        function test(){
-            $tof = true;
-            if($tof){
-                echo "function works";
-            }
-            else{
-                echo "FAILURE";
-            }
-        }
-        test();
-        echo "<br> <i> test </i> <br>";
-        echo 3 % 10;
+       error_reporting(E_ERROR | E_PARSE);
+       $user = false;
+       $pass = false;
+
+       if($_POST['name'] == "testU"){
+            $user = true;
+       }
+       if($_POST['pass'] == "testP"){
+            $pass = true;
+       }
+
+       switch([$user, $pass]){
+            case [true, true]:
+                echo "user logged in";
+                break;
+            case [true, false]:
+                echo "password incorrect";
+                break;
+            case [false, true]:
+                echo "username incorrect";
+                break;
+            default: 
+                echo "no such user";
+                break;
+       }
         
     ?>
    <!-- end PHP code --> 
 </body>
+
 </html>
